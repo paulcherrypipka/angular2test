2fc2770a3181279ff40fbb767b39f662ea1b5bcf
paulcherr <testangular@asd.com>
2016-04-05 17:23:55 +0300

    checkpoint 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
 import {HeroService} from './hero.service';
 import {HeroesComponent} from "./heroes.component";
 import {DashboardComponent} from "./dashboard.component";
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -36,6 +37,11 @@ import {DashboardComponent} from "./dashboard.component";
         name: 'Dashboard',
         component: DashboardComponent,
         useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
     }
 ])
 
